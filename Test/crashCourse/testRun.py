147157b9ddf59015567f1d0c94bcd124c6b835c4
LeagueOfFreeWorlds <knowledgelord12@gmail.com>
2021-08-30 22:31:44 -0600

    First attempt at pushing code from Eclipse

---
+++ Test/crashCourse/testRun.py
@@ -0,0 +1,11 @@
+'''
+Created on Aug 30, 2021
+
+@author: thickbook
+'''
+character_name = "Henry"
+character_age = "35"
+print("There once was a man named" + character_name, ", ")
+print("He was " + character_age + "years old.")
+print("He really disliked the name " + character_name)
+print("But he really liked that he was " + character_age + " years old")
---
+++ Test/module_test.py
@@ -0,0 +1,9 @@
+'''
+Created on Aug 30, 2021
+
+@author: Nathan Igo
+@class: CS 1400
+'''
+i = 0;
+for i in range(0, 20):
+    print("Hello Mr. Mano, pleasure to meet you.")
