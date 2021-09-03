0f6e5cf417e21544b4331a3b7462eca1d86d7990
LeagueOfFreeWorlds <knowledgelord12@gmail.com>
2021-09-02 23:18:20 -0600

    Starting work with arrays

--- Test/crashCourse/testTwo.py
+++ Test/crashCourse/testTwo.py
@@ -3,3 +3,8 @@ Created on Aug 30, 2021
 
 @author: thickbook
 '''
+# Creating an array is as simple as typing one! : 
+movies = ["The Holy Grail",
+          "The Life Of Brian",
+          "The Meaning Of Life"]
+print(movies[1])
