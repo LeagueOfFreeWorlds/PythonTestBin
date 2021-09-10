458dd16d78c8d8473e1ceb66c313a2a5df025104
LeagueOfFreeWorlds <knowledgelord12@gmail.com>
2021-09-09 23:14:48 -0600

    added re

---
+++ README.md
@@ -0,0 +1,3 @@
+# Welcome to my Python pastebin!
+I don't have much to show here, apart from the fact that this is literally just code I wrote while learning
+python.
---
+++ Test/guiCrash/Turtles.py
@@ -0,0 +1,15 @@
+'''
+Created on Sep 9, 2021
+
+@author: nathan-host
+'''
+import turtle
+
+turtle.forward(150)
+turtle.left(90)
+turtle.forward(200)
+turtle.right(45)
+turtle.forward(250)
+turtle.right(80)
+turtle.forward(300)
+turtle.done()
