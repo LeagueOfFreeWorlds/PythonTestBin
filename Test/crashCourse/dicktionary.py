5255b807b0ef1e8f84a8c5cde65bba8351450bd3
LeagueOfFreeWorlds <knowledgelord12@gmail.com>
2021-09-21 14:01:41 -0600

    added dicktionary

---
+++ Test/crashCourse/dicktionary.py
@@ -0,0 +1,18 @@
+'''
+@author: LeagueOfFreeWorlds
+Description: a dictionary... for dicks. 
+'''
+monthConversions = {
+    "Jan": "January",
+    "Feb": "February",
+    "Mar": "March",
+    "Apr": "April",
+    "May": "May",
+    "Jun": "June",
+    "Jul": "July",
+    "Aug": "August",
+    "Sep": "September",
+    "Oct": "October"
+    
+}
+print(monthConversions["Feb"])
