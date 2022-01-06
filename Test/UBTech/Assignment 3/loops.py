d413ea9224ebe431ab65cbc6f557e17475160dcf
LeagueOfFreeWorlds <knowledgelord12@gmail.com>
2022-01-06 08:09:33 -0700

    Added some UBTech python assignments

---
+++ Test/UBTech/Assignment 3/loops.py
@@ -0,0 +1,24 @@
+'''
+@author: Nathan Igo
+@date: 1/6/2022
+'''
+PassWord = ''
+FirstName = ''
+LastName = ''
+UserName = ''
+def main():
+    print("Welcome to account creation wizard. Please follow the instructions below to get your account created.")
+    print("We will be asking for your first and last name, your user name, and a 3 character password.")
+
+
+def getName():
+    FirstName = input("Please enter your first name.\t")
+    LastName = input("Please enter your last name.\t")
+    name = FirstName, LastName
+    return name
+
+def getUserName():
+
+def passCreate():
+
+main()
