845c42c63d940a63d2283e9238052b4b54dd326e
LeagueOfFreeWorlds <knowledgelord12@gmail.com>
2022-01-08 11:22:20 -0700

    Still working on UBTech

--- Test/UBTech/Assignment 3/loops.py
+++ Test/UBTech/Assignment 3/loops.py
@@ -2,23 +2,43 @@
 @author: Nathan Igo
 @date: 1/6/2022
 '''
-PassWord = ''
+'''PassWord = ''
 FirstName = ''
 LastName = ''
-UserName = ''
+UserName = '''
 def main():
     print("Welcome to account creation wizard. Please follow the instructions below to get your account created.")
     print("We will be asking for your first and last name, your user name, and a 3 character password.")
+    getUserSummary()
 
-
-def getName():
+#///////// SETTTERS: /////////////////////////////////////////
+def setName():
     FirstName = input("Please enter your first name.\t")
     LastName = input("Please enter your last name.\t")
     name = FirstName, LastName
     return name
 
-def getUserName():
-
+def setUserName():
+    userName = input("Please enter your username:\t")
+    return userName
+#/////////////////////////////////////////////////////////////
 def passCreate():
+    check == False;
+    while check == False:
+        password = input("Plase enter your password (three numerical characters ONLY!):\t")
+        for i in password:
+            if password[i] == type(str):
+                print("Please enter numerical values only.")
+                break
+        if len(password) > 3:
+            print("A password can only be up to 3 characters long. Please try again.")
+            break
+        else:
+            print("Password created successfully:\t", password)
+
+def getUserSummary():
+    setName() = fist, last
+    setUserName() = usern
+    print(first, last, usern)
 
 main()
